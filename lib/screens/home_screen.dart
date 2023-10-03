5c25dd731708a2f615fd9a27824c26b89b327440
Favour <favour@pop-os.localdomain>
2023-10-03 12:53:20 +0100

    modifying routines page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,67 +17,73 @@ class _HomeScreenState extends State<HomeScreen> {
   int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var bottomNavigation = NavigationBar(
+      elevation: 10,
+      shadowColor: Colors.black,
+      backgroundColor: Colors.white,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      selectedIndex: currentPageIndex,
+      animationDuration: const Duration(seconds: 1),
+      destinations: <Widget>[
+        const NavigationDestination(
+          selectedIcon: Icon(Icons.wysiwyg_rounded),
+          icon: Icon(
+            Icons.wysiwyg_outlined,
+            color: Colors.black54,
+          ),
+          label: 'Routine',
+        ),
+        const NavigationDestination(
+          selectedIcon: Icon(Icons.sort),
+          icon: Icon(
+            Icons.sort_outlined,
+            color: Colors.black54,
+          ),
+          label: 'Discover',
+        ),
+        NavigationDestination(
+          selectedIcon: SvgPicture.asset(
+            'assets/noun-telescope-2145896.svg',
+            height: 30,
+            width: 30,
+            colorFilter: const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+          icon: SvgPicture.asset(
+            'assets/noun-telescope-2145896.svg',
+            height: 30,
+            width: 30,
+            colorFilter:
+                const ColorFilter.mode(Colors.black54, BlendMode.srcIn),
+          ),
+          label: 'Library',
+        ),
+        const NavigationDestination(
+          selectedIcon: Icon(Icons.equalizer),
+          icon: Icon(
+            Icons.equalizer_outlined,
+            color: Colors.black54,
+          ),
+          label: 'Profile',
+        ),
+      ],
+    );
+    var statusBarHeight = MediaQuery.of(context).viewPadding.top;
     Size screenSize = MediaQuery.of(context).size;
-    double bottomNavigationBarHeight = MediaQuery.of(context).padding.bottom;
-    double screenHeight = screenSize.height - bottomNavigationBarHeight;
+    var bottomSpace = MediaQuery.of(context).viewPadding.bottom;
+    var screenHeight = screenSize.height - 55 - statusBarHeight - bottomSpace;
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: Colors.white,
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: currentPageIndex,
-        animationDuration: const Duration(seconds: 1),
-        destinations: <Widget>[
-          const NavigationDestination(
-            selectedIcon: Icon(Icons.wysiwyg_rounded),
-            icon: Icon(
-              Icons.wysiwyg_outlined,
-              color: Colors.black54,
-            ),
-            label: 'Routine',
-          ),
-          const NavigationDestination(
-            selectedIcon: Icon(Icons.sort),
-            icon: Icon(
-              Icons.sort_outlined,
-              color: Colors.black54,
-            ),
-            label: 'Discover',
-          ),
-          NavigationDestination(
-            selectedIcon: SvgPicture.asset(
-              'assets/noun-telescope-2145896.svg',
-              height: 30,
-              width: 30,
-              colorFilter:
-                  const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-            ),
-            icon: SvgPicture.asset(
-              'assets/noun-telescope-2145896.svg',
-              height: 30,
-              width: 30,
-              colorFilter:
-                  const ColorFilter.mode(Colors.black54, BlendMode.srcIn),
-            ),
-            label: 'Library',
-          ),
-          const NavigationDestination(
-            selectedIcon: Icon(Icons.equalizer),
-            icon: Icon(
-              Icons.equalizer_outlined,
-              color: Colors.black54,
-            ),
-            label: 'Profile',
-          ),
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 55,
+        child: bottomNavigation,
       ),
       body: <Widget>[
         RoutineClass(
-          screenSize: screenSize,
+          screenHeight: screenHeight,
         ),
         DiscoverClass(
           screenHeight: screenHeight,
--- lib/widgets/routine_class.dart
+++ lib/widgets/routine_class.dart
@@ -3,29 +3,79 @@ import 'package:flutter/material.dart';
 class RoutineClass extends StatelessWidget {
   const RoutineClass({
     super.key,
-    required this.screenSize,
+    required this.screenHeight,
   });
-  final Size screenSize;
+  final double screenHeight;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          height: screenSize.height * 0.15,
-          color: const Color.fromARGB(255, 211, 209, 214),
-          child: const Center(
-            child: Text(
-              'Today',
-              style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.w600,
+    double calendarHeight = screenHeight * 0.15;
+    double height = screenHeight - kToolbarHeight - calendarHeight;
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 198, 190, 211),
+      appBar: AppBar(
+        title: const Text('Today'),
+        foregroundColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 198, 190, 211),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: calendarHeight,
+            color: const Color.fromARGB(255, 198, 190, 211),
+            child: const Center(
+              child: Text(
+                'Implement Calendar Here',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
-        )
-      ],
+          Container(
+            height: height,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+            child: const Center(
+              child: Text('No Tasks For Today yet!'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        child: const Icon(
+          Icons.add_rounded,
+          size: 35,
+        ),
+      ),
     );
   }
 }
+// Column(
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: [
+//         Container(
+//           height: screenSize.height * 0.13,
+//           color: const Color.fromARGB(255, 211, 209, 214),
+//           child: const Center(
+//             child: Text(
+//               'Today',
+//               style: TextStyle(
+//                 fontSize: 17,
+//                 fontWeight: FontWeight.w600,
+//               ),
+//             ),
+//           ),
+//         )
+//       ],
+//     )
